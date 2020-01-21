a52ad7905631921efdf813f76636b85a4a7b1869
Azhar Malik <azharmalik02@gmail.com>
2020-01-21 12:32:28 +0500

    Update models and added task to update access-rights

--- lib/mix/tasks/users_task.ex
+++ lib/mix/tasks/users_task.ex
@@ -1,5 +1,6 @@
 defmodule EvercamMedia.UsersTask do
   require Logger
+  alias Evercam.Repo
 
   def update_user_keys do
     User.all
@@ -13,4 +14,19 @@ defmodule EvercamMedia.UsersTask do
     end)
   end
 
+  def update_camera_share_rights do
+    AccessRight.all
+    |> Enum.each(fn(right) ->
+      user_id = Util.deep_get(right, [:access_token, :user_id], 0)
+      case user_id do
+        id when id > 0 ->
+          Logger.info("token-id: #{right.token_id}, user-id: #{id}, new-field-user-id: #{right.user_id}")
+          right
+          |> AccessRight.changeset(%{user_id: id})
+          |> Repo.update
+        _ -> Logger.info "User id empty for right-id: #{right.id}"
+      end
+
+    end)
+  end
 end
